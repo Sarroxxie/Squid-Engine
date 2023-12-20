d3c2d0d8cc2847793aa04e045c39f7c881423ba7
Sarroxxie <josias.distler@web.de>
2023-12-20 12:10:14 +0100

    Add window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
-#include <imgui.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    auto* callbackData = glfwGetWindowUserPointer(window);
+
+    // VulkanRenderer* renderer = (VulkanRenderer*)glfwGetWindowUserPointer(window);
+    // compile shaders
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
+        std::cout << "space was pressed\n";
+    }
+}
+
+static void framebufferResizeCallback(GLFWwindow* glfWwindow, int width, int height) {
+	std::cout << "resize got called\n";
+}
+
 int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "ThesisRenderer", nullptr, nullptr);
+	glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
+	glfwSetKeyCallback(window, keyCallback);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
 	
+	// clean up resources
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
