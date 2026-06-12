13fac76be1a1da8df1bf884cc2e664685da954f3
Sarroxxie <30183651+Sarroxxie@users.noreply.github.com>
2026-06-12 09:59:24 +0200

    Begin restructuring of SwapchainBuilder
    Does not compile currently, as function implementations are still missing

--- src/presentation/SwapchainBuilder.cpp
+++ src/presentation/SwapchainBuilder.cpp
@@ -11,6 +11,7 @@ SwapchainBuilder::SwapchainBuilder(const VkPhysicalDevice& physicalDevice, const
     setSurface(surface);
 }
 
+// TODO: this should also have an option to return false!
 SwapchainBuilder& SwapchainBuilder::setPhysicalDevice(const VkPhysicalDevice& physicalDevice) {
     check(physicalDevice != VK_NULL_HANDLE, "Physical Device is invalid on Swapchain creation.");
     this->physicalDevice = physicalDevice;
@@ -34,7 +35,9 @@ SwapchainBuilder& SwapchainBuilder::setFlags(const VkSwapchainCreateFlagsKHR fla
     return *this;
 }
 
+// TODO: this should also have an option to return false!
 SwapchainBuilder& SwapchainBuilder::setSurface(const VkSurfaceKHR surface) {
+    // TODO: should we even check this here? -> yes, because we have to requery stuff....
     check(surface != VK_NULL_HANDLE, "Surface is invalid on Swapchain creation.");
     swapchainCreateInfo.surface = surface;
 
@@ -42,46 +45,71 @@ SwapchainBuilder& SwapchainBuilder::setSurface(const VkSurfaceKHR surface) {
     querySurfaceCapabilities();
     querySurfaceFormats();
     querySurfacePresentModes();
-    // TODO: need to check ALL the createInfo for support again here!
-    setMinImageCount(surfaceCapabilities.minImageCount);
+    
+    // adjust parameters to new surface
     setImageExtent(surfaceCapabilities.currentExtent);
+    if(swapchainCreateInfo.minImageCount < surfaceCapabilities.minImageCount)
+        setMinImageCount(surfaceCapabilities.minImageCount);
+    
+    check(checkCreateInfoSupport(), "Surface is lacking support for requested features.");
+
     return *this;
 }
 
-bool SwapchainBuilder::setMinImageCount(const uint32_t minImageCount) {
-    if(!checkMinImageCountSupport(minImageCount))
-        return false;
-    swapchainCreateInfo.minImageCount = minImageCount;
-    return true;
+SwapchainBuilder& SwapchainBuilder::setMinImageCount(const uint32_t minImageCount,
+                                                     bool& success) {
+    if(checkMinImageCountSupport(minImageCount)) {
+        swapchainCreateInfo.minImageCount = minImageCount;
+        success                           = true;
+    } else {
+        success = false;
+    }
+    return *this;
 }
 
-bool SwapchainBuilder::setSurfaceFormat(const VkSurfaceFormatKHR surfaceFormat) {
-    if(!checkSurfaceFormatSupport(surfaceFormat))
-        return false;
-    swapchainCreateInfo.imageFormat     = surfaceFormat.format;
-    swapchainCreateInfo.imageColorSpace = surfaceFormat.colorSpace;
-    return true;
+SwapchainBuilder& SwapchainBuilder::setSurfaceFormat(const VkSurfaceFormatKHR surfaceFormat,
+                                                     bool& success) {
+    if(checkSurfaceFormatSupport(surfaceFormat)) {
+        swapchainCreateInfo.imageFormat     = surfaceFormat.format;
+        swapchainCreateInfo.imageColorSpace = surfaceFormat.colorSpace;
+        success                             = true;
+    } else {
+        success = false;
+    }
+    return *this;
 }
 
-bool SwapchainBuilder::setImageExtent(const VkExtent2D imageExtent) {
-    if(!checkImageExtentSupport(imageExtent))
-        return false;
-    swapchainCreateInfo.imageExtent = imageExtent;
-    return true;
+SwapchainBuilder& SwapchainBuilder::setImageExtent(const VkExtent2D imageExtent,
+                                                   bool&            success) {
+    if(checkImageExtentSupport(imageExtent)) {
+        swapchainCreateInfo.imageExtent = imageExtent;
+        success                         = true;
+    } else {
+        success = false;
+    }
+    return *this;
 }
 
-bool SwapchainBuilder::setImageArrayLayers(const uint32_t imageArrayLayers) {
-    if(!checkImageArrayLayersSupport(imageArrayLayers))
-        return false;
-    swapchainCreateInfo.imageArrayLayers = imageArrayLayers;
-    return true;
+SwapchainBuilder& SwapchainBuilder::setImageArrayLayers(const uint32_t imageArrayLayers,
+                                                        bool& success) {
+    if(checkImageArrayLayersSupport(imageArrayLayers)) {
+        swapchainCreateInfo.imageArrayLayers = imageArrayLayers;
+        success                              = true;
+    } else {
+        success = false;
+    }
+    return *this;
 }
 
-bool SwapchainBuilder::setImageUsage(const VkImageUsageFlags imageUsage) {
-    if(!checkImageUsageSupport(imageUsage))
-        return false;
-    swapchainCreateInfo.imageUsage = imageUsage;
-    return true;
+SwapchainBuilder& SwapchainBuilder::setImageUsage(const VkImageUsageFlags imageUsage,
+                                                  bool& success) {
+    if(checkImageUsageSupport(imageUsage)) {
+        swapchainCreateInfo.imageUsage = imageUsage;
+        success                        = true;
+    } else {
+        success = false;
+    }
+    return *this;
 }
 
 SwapchainBuilder& SwapchainBuilder::setImageSharingMode(const VkSharingMode imageSharingMode) {
@@ -97,27 +125,36 @@ SwapchainBuilder& SwapchainBuilder::setQueueFamilyIndices(const std::vector<uint
     return *this;
 }
 
-bool SwapchainBuilder::setPreTransform(const VkSurfaceTransformFlagBitsKHR preTransform) {
-    if(!checkPreTransformSupport(preTransform))
-        return false;
-    swapchainCreateInfo.preTransform = preTransform;
-    return true;
-}
-
-bool SwapchainBuilder::setCompositeAlpha(const VkCompositeAlphaFlagBitsKHR compositeAlpha) {
-    if(!checkCompositeAlphaSupport(compositeAlpha))
-        return false;
-    swapchainCreateInfo.compositeAlpha = compositeAlpha;
-    return true;
-}
-
-bool SwapchainBuilder::setPresentMode(const VkPresentModeKHR presentMode) {
-    if(std::find(surfacePresentModes.begin(), surfacePresentModes.end(), presentMode)
-       == surfacePresentModes.end()) {
-        return false;
+SwapchainBuilder& SwapchainBuilder::setPreTransform(const VkSurfaceTransformFlagBitsKHR preTransform,
+                                                    bool& success) {
+    if(checkPreTransformSupport(preTransform)) {
+        swapchainCreateInfo.preTransform = preTransform;
+        success                          = true;
+    } else {
+        success = false;
     }
-    swapchainCreateInfo.presentMode = presentMode;
-    return true;
+    return *this;
+}
+
+SwapchainBuilder& SwapchainBuilder::setCompositeAlpha(const VkCompositeAlphaFlagBitsKHR compositeAlpha, bool& success) {
+    if(checkCompositeAlphaSupport(compositeAlpha)) {
+        swapchainCreateInfo.compositeAlpha = compositeAlpha;
+        success                          = true;
+    } else {
+        success = false;
+    }
+    return *this;
+}
+
+SwapchainBuilder& SwapchainBuilder::setPresentMode(const VkPresentModeKHR presentMode, bool& success) {
+    if(std::find(surfacePresentModes.begin(), surfacePresentModes.end(), presentMode)
+       != surfacePresentModes.end()) {
+        swapchainCreateInfo.presentMode = presentMode;
+        success = true;
+    } else {
+        success = false;
+    }
+    return *this;
 }
 
 SwapchainBuilder& SwapchainBuilder::setClipped(const VkBool32 clipped) {
@@ -130,12 +167,24 @@ SwapchainBuilder& SwapchainBuilder::setOldSwapchain(const VkSwapchainKHR oldSwap
     return *this;
 }
 
-bool SwapchainBuilder::setSwapchainCreateInfo(const VkSwapchainCreateInfoKHR swapchainCreateInfo) {
-    // TODO: need to set each field here and check on the way.... :(
-    return false;
+SwapchainBuilder& SwapchainBuilder::setSwapchainCreateInfo(const VkSwapchainCreateInfoKHR swapchainCreateInfo, bool& success) {
+    success = swapchainCreateInfo.surface != VK_NULL_HANDLE;
+    if(!success) {
+        SLOG_ERROR("Surface is invalid when attempting to set SwapchainCreateInfo.");
+        return *this;
+    } else {
+        this->swapchainCreateInfo = swapchainCreateInfo;
+        // need to re-query
+        querySurfaceCapabilities();
+        querySurfaceFormats();
+        querySurfacePresentModes();
+
+        check(checkCreateInfoSupport(), "Surface is lacking support for requested features.");
+    }
+    return *this;
 }
 
-bool SwapchainBuilder::enableVsyncPresentMode() {
+SwapchainBuilder& SwapchainBuilder::enableVsyncPresentMode(bool& success) {
     // TODO: test what happens if FIFO is active and minImageCount = 1! ->
     // concern is naming here: vsync requires more than 1 image at a time but the
     // double buffering function requires the present mode to be set correctly
@@ -144,31 +193,39 @@ bool SwapchainBuilder::enableVsyncPresentMode() {
        && swapchainCreateInfo.presentMode != VK_PRESENT_MODE_MAILBOX_KHR
        && swapchainCreateInfo.presentMode != VK_PRESENT_MODE_FIFO_RELAXED_KHR
        && swapchainCreateInfo.presentMode != VK_PRESENT_MODE_FIFO_LATEST_READY_KHR) {
-        if(!setPresentMode(VK_PRESENT_MODE_FIFO_KHR)) {
+        setPresentMode(VK_PRESENT_MODE_FIFO_KHR, success);
+        if(!success) {
             SLOG_ERROR("Failed to set Swapchain present mode to VK_PRESENT_MODE_FIFO_KHR when attempting to enable Vsync.");
-            return false;
+        } else {
+            SLOG_INFO("Set Swapchain present mode to VK_PRESENT_MODE_FIFO_KHR when enabling Vsync.");
         }
-        SLOG_INFO("Set Swapchain present mode to VK_PRESENT_MODE_FIFO_KHR when enabling Vsync.");
+    } else {
+        success = true;
     }
-    return true;
+    return *this;
 }
 
-bool SwapchainBuilder::setToDoubleBuffering() {
-    if(!setMinImageCount(2)) {
+SwapchainBuilder& SwapchainBuilder::setToDoubleBuffering(bool& success) {
+    setMinImageCount(2, success);
+    if(!success) {
         SLOG_ERROR("Failed to set Swapchain minImageCount to 2 when attempting to enable double buffering.");
-        return false;
+        return *this;
+    } else {
+        SLOG_INFO("Set Swapchain minImageCount to 2 to enable double buffering.");
+        enableVsyncPresentMode(success);
     }
-    SLOG_INFO("Set Swapchain minImageCount to 2 to enable double buffering.");
-    return enableVsyncPresentMode();
+    return *this;
 }
 
-bool SwapchainBuilder::setToTripleBuffering() {
-    if(!setMinImageCount(3)) {
+SwapchainBuilder& SwapchainBuilder::setToTripleBuffering(bool& success) {
+    setMinImageCount(2, success);
+    if(!success) {
         SLOG_ERROR("Failed to set Swapchain minImageCount to 3 when attempting to enable triple buffering.");
-        return false;
+    } else {
+        SLOG_INFO("Set Swapchain minImageCount to 3 to enable triple buffering.");
+        enableVsyncPresentMode();
     }
-    SLOG_INFO("Set Swapchain minImageCount to 3 to enable triple buffering.");
-    return enableVsyncPresentMode();
+    return *this;
 }
 
 void SwapchainBuilder::querySurfaceCapabilities() {
@@ -200,6 +257,22 @@ void SwapchainBuilder::querySurfacePresentModes() {
                                                     swapchainCreateInfo.surface, &presentModeCount,
                                                     presentModes.data()),
           "Failed to query Surface Present Modes on Swapchain Creation.");
+}
+
+bool SwapchainBuilder::checkCreateInfoSupport() const {
+    bool success = true;
+
+    success = success && checkMinImageCountSupport(swapchainCreateInfo.minImageCount);
+    success = success
+              && checkSurfaceFormatSupport(VkSurfaceFormatKHR{
+                  swapchainCreateInfo.imageFormat, swapchainCreateInfo.imageColorSpace});
+    success = success && checkImageExtentSupport(swapchainCreateInfo.imageExtent);
+    success = success && checkImageArrayLayersSupport(swapchainCreateInfo.imageArrayLayers);
+    success = success && checkImageUsageSupport(swapchainCreateInfo.imageUsage);
+    success = success && checkPreTransformSupport(swapchainCreateInfo.preTransform);
+    success = success && checkCompositeAlphaSupport(swapchainCreateInfo.compositeAlpha);
+    success = success && checkPresentModeSupport(swapchainCreateInfo.presentMode);
+    return success;
 }
 
 bool SwapchainBuilder::checkMinImageCountSupport(uint32_t minImageCount) const {
--- src/presentation/SwapchainBuilder.h
+++ src/presentation/SwapchainBuilder.h
@@ -12,8 +12,12 @@
 class SwapchainBuilder
 {
   public:
+    // TODO: decide on a better ordering of functions here -> build first, then physicalDevice + surface, then rest
+    // TODO: implement the function variant that do not take a "bool& success" as parameter
+
     SwapchainBuilder();
     SwapchainBuilder(const VkPhysicalDevice& physicalDevice, const VkSurfaceKHR& surface);
+    // may throw exception
     SwapchainBuilder& setPhysicalDevice(const VkPhysicalDevice& physicalDevice);
     // TODO: documentation! -> needs valid physical device and surface
     void build(const VkDevice& device, VkSwapchainKHR& swapchain) const;
@@ -22,27 +26,58 @@ class SwapchainBuilder
     // chaining? this would match the behavior of the InstanceBuilder and the DeviceBuilder
     // => YES! instead of bool outputs it should throw a runtime error! -> or instead define new exceptions!
 
+    // idea: make it optional to get feedback if things worked -> optional "bool& success" -> two versions of functions
+
     // checks parameters for support and set fields inside the VkSwapchainCreateInfoKHR
     SwapchainBuilder& setFlags(const VkSwapchainCreateFlagsKHR flags);
+    // may throw exception
     SwapchainBuilder& setSurface(const VkSurfaceKHR surface);
-    bool setMinImageCount(const uint32_t minImageCount);
-    bool setSurfaceFormat(const VkSurfaceFormatKHR surfaceFormat);
-    bool setImageExtent(const VkExtent2D imageExtent);
-    bool setImageArrayLayers(const uint32_t imageArrayLayers);
-    bool setImageUsage(const VkImageUsageFlags imageUsage);
+
+    SwapchainBuilder& setMinImageCount(const uint32_t minImageCount, bool& success);
+    SwapchainBuilder& setMinImageCount(const uint32_t minImageCount);
+
+    SwapchainBuilder& setSurfaceFormat(const VkSurfaceFormatKHR surfaceFormat, bool& success);
+    SwapchainBuilder& setSurfaceFormat(const VkSurfaceFormatKHR surfaceFormat);
+
+    SwapchainBuilder& setImageExtent(const VkExtent2D imageExtent, bool& success);
+    SwapchainBuilder& setImageExtent(const VkExtent2D imageExtent);
+
+    SwapchainBuilder& setImageArrayLayers(const uint32_t imageArrayLayers, bool& success);
+    SwapchainBuilder& setImageArrayLayers(const uint32_t imageArrayLayers);
+
+    SwapchainBuilder& setImageUsage(const VkImageUsageFlags imageUsage, bool& success);
+    SwapchainBuilder& setImageUsage(const VkImageUsageFlags imageUsage);
+
     SwapchainBuilder& setImageSharingMode(const VkSharingMode imageSharingMode);
     SwapchainBuilder& setQueueFamilyIndices(const std::vector<uint32_t> queueFamilyIndices);
-    bool setPreTransform(const VkSurfaceTransformFlagBitsKHR preTransform);
-    bool setCompositeAlpha(const VkCompositeAlphaFlagBitsKHR compositeAlpha);
-    bool setPresentMode(const VkPresentModeKHR presentMode);
+
+
+    SwapchainBuilder& setPreTransform(const VkSurfaceTransformFlagBitsKHR preTransform,
+                                      bool& success);
+    SwapchainBuilder& setPreTransform(const VkSurfaceTransformFlagBitsKHR preTransform);
+
+
+    SwapchainBuilder& setCompositeAlpha(const VkCompositeAlphaFlagBitsKHR compositeAlpha, bool& success);
+    SwapchainBuilder& setCompositeAlpha(const VkCompositeAlphaFlagBitsKHR compositeAlpha);
+
+    SwapchainBuilder& setPresentMode(const VkPresentModeKHR presentMode, bool& success);
+    SwapchainBuilder& setPresentMode(const VkPresentModeKHR presentMode);
+
     SwapchainBuilder& setClipped(const VkBool32 clipped);
     SwapchainBuilder& setOldSwapchain(const VkSwapchainKHR oldSwapchain);
 
-    bool setSwapchainCreateInfo(const VkSwapchainCreateInfoKHR swapchainCreateInfo);
+    // may throw exception
+    SwapchainBuilder& setSwapchainCreateInfo(const VkSwapchainCreateInfoKHR swapchainCreateInfo, bool& success);
+    SwapchainBuilder& setSwapchainCreateInfo(const VkSwapchainCreateInfoKHR swapchainCreateInfo);
 
-    bool enableVsyncPresentMode();
-    bool setToDoubleBuffering();
-    bool setToTripleBuffering();
+    SwapchainBuilder& enableVsyncPresentMode(bool& success);
+    SwapchainBuilder& enableVsyncPresentMode();
+
+    SwapchainBuilder& setToDoubleBuffering(bool& success);
+    SwapchainBuilder& setToDoubleBuffering();
+
+    SwapchainBuilder& setToTripleBuffering(bool& success);
+    SwapchainBuilder& setToTripleBuffering();
 
     // TODO: do we need getters to get the current values?
 
@@ -72,6 +107,8 @@ class SwapchainBuilder
     void querySurfaceFormats();
     void querySurfacePresentModes();
 
+    // TODO: checks if the content of the create info is supported
+    bool checkCreateInfoSupport() const;
     bool checkMinImageCountSupport(uint32_t minImageCount) const;
     bool checkSurfaceFormatSupport(VkSurfaceFormatKHR imageFormat) const;
     bool checkImageExtentSupport(VkExtent2D imageExtent) const;
