01a3107b0d67c705891e369e86e74cb123cdd5ab
Sarroxxie <30183651+Sarroxxie@users.noreply.github.com>
2026-06-17 15:44:52 +0200

    Fix API version

--- src/setup/InstanceBuilder.h
+++ src/setup/InstanceBuilder.h
@@ -78,7 +78,7 @@ class InstanceBuilder
                                       .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
                                       .pEngineName   = "Engine",
                                       .engineVersion = VK_MAKE_VERSION(1, 0, 0),
-                                      .apiVersion    = VK_API_VERSION_1_3};
+                                      .apiVersion    = VK_API_VERSION_1_4};
 
     VkInstanceCreateInfo createInfo{.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
                                     .pApplicationInfo = &applicationInfo};
