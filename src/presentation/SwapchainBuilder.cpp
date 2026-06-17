a4b86c265304caf4fa439057bc3f01051b2d9ab9
Sarroxxie <30183651+Sarroxxie@users.noreply.github.com>
2026-06-17 15:29:31 +0200

    Add Image Views for Swapchain

--- src/presentation/SwapchainBuilder.cpp
+++ src/presentation/SwapchainBuilder.cpp
@@ -29,19 +29,36 @@ Swapchain SwapchainBuilder::build(const VkDevice& device) const {
     // extracting swapchain images
     uint32_t swapchainImageCount;
     // TODO: need to insert "check" functions here!
-    vkGetSwapchainImagesKHR(device, swapchain.handle, &swapchainImageCount, nullptr);
+    check(vkGetSwapchainImagesKHR(device, swapchain.handle, &swapchainImageCount, nullptr), "INSERT MESSAGE HERE TODO");
     swapchain.images.resize(swapchainImageCount);
-    vkGetSwapchainImagesKHR(device, swapchain.handle, &swapchainImageCount,
-                            swapchain.images.data());
+    check(vkGetSwapchainImagesKHR(device, swapchain.handle, &swapchainImageCount, swapchain.images.data()),
+          "INSERT MESSAGE HERE TODO");
     swapchain.imageFormat = swapchainCreateInfo.imageFormat;
     swapchain.extent      = swapchainCreateInfo.imageExtent;
 
+    // TODO: finish implementation! -> should write an abstraction for image view creation!
 
     // creating image views for swapchain images
     swapchain.imageViews.resize(swapchain.images.size());
-    VkImageViewCreateInfo imageViewCreateInfo{VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO};
-    // TODO: finish implementation!
+    VkImageViewCreateInfo createInfo{VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO};
+    createInfo.viewType                        = VK_IMAGE_VIEW_TYPE_2D;
+    createInfo.format                          = swapchain.imageFormat;
+    createInfo.components.r                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+    createInfo.components.g                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+    createInfo.components.b                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+    createInfo.components.a                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+    createInfo.subresourceRange.aspectMask     = VK_IMAGE_ASPECT_COLOR_BIT;
+    createInfo.subresourceRange.baseMipLevel   = 0;
+    createInfo.subresourceRange.levelCount     = 1;
+    createInfo.subresourceRange.baseArrayLayer = 0;
+    createInfo.subresourceRange.layerCount     = 1;
 
+    for(size_t i = 0; i < swapchain.images.size(); i++) {
+        createInfo.image        = swapchain.images[i];
+
+        check(vkCreateImageView(device, &createInfo, nullptr, &swapchain.imageViews[i]),
+              "Failed to create Image View after fetching Swapchain Images.");
+    }
 
     return swapchain;
 }
