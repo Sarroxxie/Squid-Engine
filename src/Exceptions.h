0abd7c6cc68f1e4d6beaf48f277f70d24571c5c4
Sarroxxie <30183651+Sarroxxie@users.noreply.github.com>
2026-07-04 17:58:56 +0200

    Use macro for exception creation

--- src/Exceptions.h
+++ src/Exceptions.h
@@ -3,72 +3,38 @@
 #include <stdexcept>
 
 /*
-Error messages are of format "prefix + connector + message"
-The connector is shared for all exceptions while the prefix is custom per exception type.
+Usage of exceptions: Choose a ClassName that describes WHERE (while doing which
+step) the error occurred and use the message to describe WHAT exactly went wrong.
+Example: ClassName = SwapchainCreationException
+         message   = "Failed to create Image View after fetching Swapchain Image."
+*/
+
+/*
+Error messages are of format "ClassName" + CONNECTOR + message
+The connector is shared for all exceptions.
 */
 
 constexpr char CONNECTOR[] = ": ";
 
-constexpr char INSTANCE_CREATION_PREFIX[]         = "Instance Creation";
-constexpr char SURFACE_CREATION_PREFIX[]          = "Surface Creation";
-constexpr char PHYSICAL_DEVICE_SELECTION_PREFIX[] = "Physical Device Selection";
-constexpr char DEVICE_CREATION_PREFIX[]           = "Device Creation";
-constexpr char SWAPCHAIN_CREATION_PREFIX[]        = "Swapchain Creation";
-constexpr char DEBUG_UTILS_MESSENGER_CREATION_PREFIX[] = "Debug Utils Messenger Creation";
-constexpr char QUEUE_FAMILY_INDEX_PREFIX[] = "Queue Family Index";
 
-class InstanceCreationException : public std::runtime_error
-{
-  public:
-    InstanceCreationException(const std::string& message)
-        : std::runtime_error(
-              (INSTANCE_CREATION_PREFIX + std::string(CONNECTOR) + message).c_str()) {};
-};
+/*
+Use this Macro to generate different classes of exceptions (that extend
+"std::runtime_error") and prepend their name as well as the "CONNECTOR" to the
+error message. "ExceptionName" MUST be valid as a class name (use CamelCase)!
+*/
+#define GENERATE_EXCEPTION(ExceptionName)                                          \
+    class ExceptionName : public std::runtime_error                                \
+    {                                                                              \
+      public:                                                                      \
+        ExceptionName(const std::string& message)                                  \
+            : std::runtime_error(                                                  \
+                  (#ExceptionName + std::string(CONNECTOR) + message).c_str()) {}; \
+    }
 
-class SurfaceCreationException : public std::runtime_error
-{
-  public:
-    SurfaceCreationException(const std::string& message)
-        : std::runtime_error(
-              (SURFACE_CREATION_PREFIX + std::string(CONNECTOR) + message).c_str()) {};
-};
-
-class PhysicalDeviceSelectionException : public std::runtime_error
-{
-  public:
-    PhysicalDeviceSelectionException(const std::string& message)
-        : std::runtime_error((PHYSICAL_DEVICE_SELECTION_PREFIX + std::string(CONNECTOR) + message)
-                                 .c_str()) {};
-};
-
-class DeviceCreationException : public std::runtime_error
-{
-  public:
-    DeviceCreationException(const std::string& message)
-        : std::runtime_error(
-              (DEVICE_CREATION_PREFIX + std::string(CONNECTOR) + message).c_str()) {};
-};
-
-class SwapchainCreationException : public std::runtime_error
-{
-  public:
-    SwapchainCreationException(const std::string& message)
-        : std::runtime_error(
-              (SWAPCHAIN_CREATION_PREFIX + std::string(CONNECTOR) + message).c_str()) {};
-};
-
-class DebugUtilsMessengerCreationException : public std::runtime_error
-{
-  public:
-    DebugUtilsMessengerCreationException(const std::string& message)
-        : std::runtime_error((DEBUG_UTILS_MESSENGER_CREATION_PREFIX + std::string(CONNECTOR) + message)
-                                 .c_str()) {};
-};
-
-class QueueFamiliyIndexException : public std::runtime_error
-{
-  public:
-    QueueFamiliyIndexException(const std::string& message)
-        : std::runtime_error(
-              (QUEUE_FAMILY_INDEX_PREFIX + std::string(CONNECTOR) + message).c_str()) {};
-};
+GENERATE_EXCEPTION(InstanceCreationException);
+GENERATE_EXCEPTION(SurfaceCreationException);
+GENERATE_EXCEPTION(PhysicalDeviceSelectionException);
+GENERATE_EXCEPTION(DeviceCreationException);
+GENERATE_EXCEPTION(SwapchainCreationException);
+GENERATE_EXCEPTION(DebugUtilsMessengerCreationException);
+GENERATE_EXCEPTION(QueueFamiliyIndexException);
